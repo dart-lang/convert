dart-lang/convert

commit 0310659f8a0127eb8d74805a8a7ceb9f448f1acd

Added IdentityCodec (#17)
Added IdentityCodec

lib/src/identity_codec.dart
@@ -1,0 +1,28 @@
+import 'dart:convert';
+
+class _IdentityConverter<T> extends Converter<T, T> {
+  const _IdentityConverter();
+  T convert(T input) => input;
+}
+
+/// A [Codec] that performs the identity conversion (changing nothing) in both
+/// directions.
+///
+/// The identity codec passes input directly to output in both directions.
+/// This class can be used as a base when combining multiple codecs,
+/// because fusing the identity codec with any other codec gives the other
+/// codec back.
+///
+/// Note, that when fused with another [Codec] the identity codec disppears.
+class IdentityCodec<T> extends Codec<T, T> {
+  const IdentityCodec();
+
+  Converter<T, T> get decoder => _IdentityConverter<T>();
+  Converter<T, T> get encoder => _IdentityConverter<T>();
+
+  /// Fuse with an other codec.
+  ///
+  /// Fusing with the identify converter is a no-op, so this always return
+  /// [other].
+  Codec<T, R> fuse<R>(Codec<T, R> other) => other;
+}

lib/convert.dart
@@ -7,5 +7,6 @@ library convert;
 export 'src/accumulator_sink.dart';
 export 'src/byte_accumulator_sink.dart';
 export 'src/hex.dart';
+export 'src/identity_codec.dart';
 export 'src/percent.dart';
 export 'src/string_accumulator_sink.dart';

pubspec.yaml
@@ -1,5 +1,5 @@
 name: convert
-version: 2.0.2
+version: 2.1.0
 description: Utilities for converting between data representations.
 author: Dart Team <misc@dartlang.org>
 homepage: https://github.com/dart-lang/convert

test/identity_codec_test.dart
@@ -1,0 +1,23 @@
+import 'dart:convert';
+import 'package:convert/convert.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('IdentityCodec', () {
+    test('encode', () {
+      final codec = IdentityCodec<String>();
+      expect(codec.encode('hello-world'), equals('hello-world'));
+    });
+
+    test('decode', () {
+      final codec = IdentityCodec<String>();
+      expect(codec.decode('hello-world'), equals('hello-world'));
+    });
+
+    test('fuse', () {
+      final stringCodec = IdentityCodec<String>();
+      final utf8Strings = stringCodec.fuse(utf8);
+      expect(utf8Strings, equals(utf8));
+    });
+  });
+}

commit a4c1d76e3c986ab3b86025917a595191a32408b1

Make _IdentityConverter compatible with Dart SDK 1.x
+ Fixes DDC compilation regression introduced by #17

lib/src/identity_codec.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 class _IdentityConverter<T> extends Converter<T, T> {
-  const _IdentityConverter();
+  _IdentityConverter();
   T convert(T input) => input;
 }
 
@@ -17,8 +17,8 @@ class _IdentityConverter<T> extends Converter<T, T> {
 class IdentityCodec<T> extends Codec<T, T> {
   const IdentityCodec();
 
-  Converter<T, T> get decoder => _IdentityConverter<T>();
-  Converter<T, T> get encoder => _IdentityConverter<T>();
+  Converter<T, T> get decoder => new _IdentityConverter<T>();
+  Converter<T, T> get encoder => new _IdentityConverter<T>();
 
   /// Fuse with an other codec.
   ///